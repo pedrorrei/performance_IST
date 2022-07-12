6105d067a943744415a8a8aa049443f9034eba99
Pedro Rei <93801715+pedrorrei@users.noreply.github.com>
2022-07-12 01:47:49 +0100

    Update calculo1.js

--- calculo1.js
+++ calculo1.js
@@ -1,20 +1,18 @@
 function Calcula1() {
-    var v1 = document.getElementById('QFE').value;
-    var v2 = document.getElementById('el').value;
-    var res = document.getElementById('res');
-    // var x =  parseFloat((101235 * (1- 0.0065 * (((- (parseFloat(v1)) / 101325)**(1/5.25588) + 1)*(288.15 / 0.0065) + (parseFloat(v2)) )/288.15)**5.25588));
-    var x = (parseFloat(v1) / 101325)**(1/5.25588) * (-1) + 1;
-    var y = (288.15 / 0.0065) - parseFloat(v2);
-    var z = (x * y) / 288.15;
-    var w =  101235 * ((1-0.0065) * z) ** 5.25588;
-   res.value = w;
-
-
+        var x = (parseFloat(v1) / 101325);
+    var k = Math.pow(x,0.19026309) * (-1) + 1;
+    var y = (288.15 / 0.0065)*k ;
+    var z = (y - parseFloat(v2))/ 288.15;
+    var t = 1-(0.0065) * z;
+    var w =  101235 * Math.pow(t, 5.25588);
+    res.value = w;
 }
 function Calcula2() {
     var v11 = document.getElementById('EL').value;
     var v21 = document.getElementById('PA').value;
     var res2 = document.getElementById('res2');
+    var v22 = (1- 0.0065  * ((-1) *parseFloat(v11)+parseFloat(v21))/288.15);
     console.log(res2.value);
-    res2.value= 101235 * (1- 0.0065  * (-1 *parseFloat(v11)+parseFloat(v21))/288.15)^5.25588;
+    res2.value= 101235 * Math.pow(v22,5.25588);
+
 }
