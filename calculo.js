5b54572caf9174150b09eb9b9c8b41165944a26a
Pedro Rei <93801715+pedrorrei@users.noreply.github.com>
2022-07-12 02:14:24 +0100

    Update calculo.js

--- calculo.js
+++ calculo.js
@@ -5,7 +5,7 @@ function Calcula1() {
     var t = (parseFloat(v1) / 101325);
     var x = Math.pow(t, 0.19026309) * (-1) + 1;
     var y = ((288.15 / 0.0065)*x + parseFloat(v2))/288.15;
-    var z = (1-(0.0065 * z);           
+    var z = (1-(0.0065 * z));           
     var w =  101235 * Math.pow(z,5.25588);
     res.value = w;
 }
