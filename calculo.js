bb0f1a93e2d1d412269adc67cdf3c939a4bddadf
Pedro Rei <93801715+pedrorrei@users.noreply.github.com>
2022-07-16 15:53:31 +0100

    Update calculo.js

--- calculo.js
+++ calculo.js
@@ -3,7 +3,7 @@ function Calcula1() {
     var v2 = document.getElementById('ELEVATION').value;
     var res = document.getElementById('res');
     var t = (parseFloat(v1) / 101325);
-    if (v1 == null & v2 == null )
+    if (v1 == null )
 alert("Insira um Valor");
 
     console.log(t);
